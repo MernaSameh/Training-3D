258b88dc70fd54813db8fa4c4ff2c71234617ab7
Mark Sameh <mark.sameh@dsnci.com>
2023-12-06 19:53:12 +0200

    MainFiles

---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public Transform prefap;
+    public Sprite sprite;
+    public string objectName;
+}
